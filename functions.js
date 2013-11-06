5d0173b3d4bdc420be31215cc0ca5c14e8448c7d
Kaylea Nelson <Kaylea@Astrid.local>
2013-11-06 02:00:41 -0500

    initial build of homepage

---
+++ functions.js
@@ -0,0 +1,58 @@
+
+$( document ).ready(function(){
+	$("div#documentation span").click(function() { //When trigger is clicked...
+		
+		$("div#documentation ul").slideDown('fast').show(); //Drop down the subnav on click
+
+		$(this).parent().hover(function() {
+		}, function(){	
+			$(this).parent().find("ul").slideUp('slow'); //When the mouse hovers out of the subnav, move it back up
+		});
+
+
+		//Following events are applied to the trigger (Hover events for the trigger)
+		}).hover(function() { 
+			$(this).addClass("subhover"); //On hover over, add class "subhover"
+		}, function(){	//On Hover Out
+			$(this).removeClass("subhover"); //On hover out, remove class "subhover"
+
+	});
+
+
+$('#os-selector ul').each(function(){
+  // For each set of tabs, we want to keep track of
+  // which tab is active and it's associated content
+  var $active, $content, $links = $(this).find('a');
+
+  // If the location.hash matches one of the links, use that as the active tab.
+  // If no match is found, use the first link as the initial active tab.
+  $active = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
+  $active.addClass('active');
+  $content = $($active.attr('href'));
+
+  // Hide the remaining content
+  $links.not($active).each(function () {
+    $($(this).attr('href')).hide();
+  });
+
+  // Bind the click event handler
+  $(this).on('click', 'a', function(e){
+    // Make the old tab inactive.
+    $active.removeClass('active');
+    $content.hide();
+
+    // Update the variables with the new link and content
+    $active = $(this);
+    $content = $($(this).attr('href'));
+
+    // Make the tab active.
+    $active.addClass('active');
+    $content.show();
+
+    // Prevent the anchor's default click action
+    e.preventDefault();
+  });
+});
+
+
+});
