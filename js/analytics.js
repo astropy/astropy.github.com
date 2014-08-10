e03eff90cbbdd9bcd5e2de892b17ab57d36d39d1
Erik Tollerud <erik.tollerud@gmail.com>
2014-08-10 12:15:57 -0400

    add extra ga stuff

--- js/analytics.js
+++ js/analytics.js
@@ -4,4 +4,6 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-30968842-1', 'auto');
+ga('require', 'displayfeatures');
+ga('require', 'linkid', 'linkid.js');
 ga('send', 'pageview');
